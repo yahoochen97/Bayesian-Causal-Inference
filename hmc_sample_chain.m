a6965521332a8e5a18839664f65988b8de81033d
Yehu Chen <chenyehu@wustl.edu>
2021-05-26 23:39:34 -0500

    tune sampler

--- hmc_sample_chain.m
+++ hmc_sample_chain.m
@@ -2,14 +2,14 @@ function hmc_sample_chain(seed)
     addpath("../CNNForecasting/gpml-matlab-v3.6-2015-07-07");
     % addpath("/Users/yahoo/Documents/WashU/CSE515T/Code/Gaussian Process/gpml-matlab-v3.6-2015-07-07");
     startup;
+    
+    load("tunesampler.mat");
 
     % sampler parameters
     % num_chains  = 5;
-    num_samples = 1000;
-    burn_in     = 500;
-    jitter      = 0.1;
-
-    load("tunesampler.mat");
+    num_samples = 3000;
+    burn_in     = 1000;
+    jitter      = 1e-2;
 
     i = seed;
     rng(i);
@@ -26,18 +26,18 @@ function hmc_sample_chain(seed)
     save("results/drifthmc" + int2str(i) + ".mat");
 end
 
-for i=1:5
-   load("results/drifthmc" + int2str(i) + ".mat");
-   chains{i} = chain;
-end
-
-diagnostics(hmc, chains);
-samples = vertcat(chains{2});
-
-c = exp(samples);
-c(:, 3) = 2 * normcdf(samples(:, 3)) - 1;
-
-figure(3);
-clf;
-plotmatrix(c);
+% for i=1:5
+%    load("results/drifthmc" + int2str(i) + ".mat");
+%    chains{i} = chain;
+% end
+% 
+% diagnostics(hmc, chains);
+% samples = vertcat(chains{2});
+% 
+% c = exp(samples);
+% c(:, 3) = 2 * normcdf(samples(:, 3)) - 1;
+% 
+% figure(3);
+% clf;
+% plotmatrix(c);
 
--- tunesampler.m
+++ tunesampler.m
@@ -208,22 +208,22 @@ theta = minimize_v2(theta, @gp, p, inference_method, mean_function, ...
 % make_intermediate_plot_unitw;
 
 
-% oss = linspace(0.01,0.1,10);
-% lss = linspace(5,50,10);
-% ts = linspace(0,90,10)+90;
-% nlzs = zeros(10);
-% for i=1:10
-%    for j=1:10
-%       tmp = theta;
-% %       tmp.cov(14) = ts(i);
-%       tmp.cov(15) = log(lss(j));
+oss = linspace(0.01,0.1,10);
+lss = linspace(5,50,10);
+ts = linspace(0,90,10)+90;
+nlzs = zeros(10);
+for i=1:10
+   for j=1:10
+      tmp = theta;
+      tmp.cov(14) = ts(i);
+      tmp.cov(15) = log(lss(j));
 %       tmp.cov(16) = log(oss(i));
-%       nlzs(i,j)=gp(tmp,inference_method, mean_function,covariance_function,[],x,y);
-%    end
-% end
-% figure(3);
-% clf;
-% heatmap(nlzs, 'XData', lss, 'YData', oss);
+      nlzs(i,j)=gp(tmp,inference_method, mean_function,covariance_function,[],x,y);
+   end
+end
+figure(3);
+clf;
+heatmap(nlzs, 'XData', lss, 'YData', ts);
 
 % sampler parameters
 num_chains  = 5;
