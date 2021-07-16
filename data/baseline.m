ff4cc65b8b3149decc0cf00c31e298445bed9706
Yehu Chen <chenyehu@wustl.edu>
2021-07-16 16:21:24 -0500

    synthetic

--- data/baseline.m
+++ data/baseline.m
@@ -1,3 +1,4 @@
+function baseline(SEED, unit_length_scale, rho)
 % change gpml path
 addpath("../CNNForecasting/gpml-matlab-v3.6-2015-07-07");
 addpath("./model");
@@ -230,3 +231,4 @@ writetable(results((treatment_day+1):end,:),"data/synthetic/multigp_" + HYP + "_
 %                   'numprint', 10);
 %     toc;
 % end
+end
