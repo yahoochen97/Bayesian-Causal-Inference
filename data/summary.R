fa3c72d96188da662b7fb26c3fc0318527b0e9ef
Yehu Chen <chenyehu@wustl.edu>
2021-07-22 18:42:37 -0500

    synthetic

--- data/summary.R
+++ data/summary.R
@@ -8,23 +8,18 @@ MAXSEED = as.integer(args[1])
 inputs = as.double(args[-1])
 ULS = as.integer(inputs[inputs>=1])
 RHOS = as.numeric(inputs[inputs<1])
-print(MAXSEED)
-print(ULS)
-print(RHOS)
 
 MODELS = c("multigp", "ife", "tfe")
-results = data.frame(matrix(ncol = 6, nrow = 0))
-colnames(results) <- c("uls", "rho", "model", "TYPE", "seed", "measure")
+results = data.frame(matrix(ncol = 5, nrow = 0))
+colnames(results) <- c("uls", "rho", "model", "TYPE", "measure")
 for (uls in ULS) {
   for (rho in RHOS) {
-    for (SEED in 1:MAXSEED){
-      HYP = paste("rho_", sub("\\.", "", toString(rho)) , '_uls_', uls, '_SEED_', SEED,  sep="")
+      HYP = paste("rho_", sub("\\.", "", toString(rho)) , '_uls_', uls, '_SEED_', MAXSEED,  sep="")
       measures = read.csv(paste("measure", "_", HYP, ".csv", sep=""))
       measures$X = as.character(measures$X)
       for(TYPE in measures$X){
         for(MODEL in MODELS){
-          results[nrow(results)+1,] = c(uls, rho, MODEL, TYPE, SEED, measures[measures$X==TYPE, MODEL])
-        }
+          results[nrow(results)+1,] = c(uls, rho, MODEL, TYPE, measures[measures$X==TYPE, MODEL])
       }
     }
   }
@@ -35,9 +30,9 @@ results$rho = as.numeric(results$rho)
 results$seed = as.numeric(results$seed)
 results$measure = as.numeric(results$measure)
 
-results = results %>%
-  group_by(uls, rho, model, TYPE) %>%
-  summarise(measure=mean(measure))
+# results = results %>%
+#   group_by(uls, rho, model, TYPE) %>%
+#   summarise(measure=mean(measure))
 
 for(TYPE in measures$X){
   for (uls in ULS){
