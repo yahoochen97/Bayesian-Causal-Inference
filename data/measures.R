aee17b74ab069ebcbf4dda11f2f911d4bc435ba2
Yehu Chen <chenyehu@wustl.edu>
2021-08-12 13:56:41 -0500

    t test

--- data/measures.R
+++ data/measures.R
@@ -102,40 +102,47 @@ for(i in 1:length(MODELS)){
 
 print(paste("correlation: ", RHO , sep=""))
 for (i in 2:length(MODELS)) {
-  tmp = t.test(ENORMSE[,1], ENORMSE[, i], paired = TRUE)
-  p = tmp[["p.value"]]
-  if (p>=0.05){
-    print(paste(MODELS[i], " not significant worse in ENORMSE.", sep=""))
-  }
-  tmp = t.test(RMSE[,1], RMSE[, i], paired = TRUE)
-  p = tmp[["p.value"]]
-  if (p>=0.05){
-    print(paste(MODELS[i], " not significant worse in RMSE.", sep=""))
-  }
-  tmp = t.test(COVERAGE[,1], COVERAGE[, i], paired = TRUE)
-  p = tmp[["p.value"]]
-  if (p>=0.05){
-    print(paste(MODELS[i], " not significant worse in COVERAGE.", sep=""))
-  }
-  tmp = t.test(ENCIS[,1], ENCIS[, i], paired = TRUE)
-  p = tmp[["p.value"]]
-  if (p>=0.05){
-    print(paste(MODELS[i], " not significant worse in ENCISE.", sep=""))
-  }
   tmp = t.test(LL[,1], LL[, i], paired = TRUE)
   p = tmp[["p.value"]]
-  if (p>=0.05){
-    print(paste(MODELS[i], " not significant worse in LL.", sep=""))
-  }
+  print(tmp)
 }
 
-for (i in c(1,3,4,5,6,7,8)) {
-  tmp = t.test(BIAS[,2], BIAS[, i], paired = TRUE)
-  p = tmp[["p.value"]]
-  if (p>=0.05){
-    print(paste(MODELS[i], " not significant worse in BIAS.", sep=""))
-  }
-}
+# print(paste("correlation: ", RHO , sep=""))
+# for (i in 2:length(MODELS)) {
+#   tmp = t.test(ENORMSE[,1], ENORMSE[, i], paired = TRUE)
+#   p = tmp[["p.value"]]
+#   if (p>=0.05){
+#     print(paste(MODELS[i], " not significant worse in ENORMSE.", sep=""))
+#   }
+#   tmp = t.test(RMSE[,1], RMSE[, i], paired = TRUE)
+#   p = tmp[["p.value"]]
+#   if (p>=0.05){
+#     print(paste(MODELS[i], " not significant worse in RMSE.", sep=""))
+#   }
+#   tmp = t.test(COVERAGE[,1], COVERAGE[, i], paired = TRUE)
+#   p = tmp[["p.value"]]
+#   if (p>=0.05){
+#     print(paste(MODELS[i], " not significant worse in COVERAGE.", sep=""))
+#   }
+#   tmp = t.test(ENCIS[,1], ENCIS[, i], paired = TRUE)
+#   p = tmp[["p.value"]]
+#   if (p>=0.05){
+#     print(paste(MODELS[i], " not significant worse in ENCISE.", sep=""))
+#   }
+#   tmp = t.test(LL[,1], LL[, i], paired = TRUE)
+#   p = tmp[["p.value"]]
+#   if (p>=0.05){
+#     print(paste(MODELS[i], " not significant worse in LL.", sep=""))
+#   }
+# }
+# 
+# for (i in c(1,3,4,5,6,7,8)) {
+#   tmp = t.test(BIAS[,2], BIAS[, i], paired = TRUE)
+#   p = tmp[["p.value"]]
+#   if (p>=0.05){
+#     print(paste(MODELS[i], " not significant worse in BIAS.", sep=""))
+#   }
+# }
 
 
 ENORMSE = colMeans(ENORMSE)
