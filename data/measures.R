4e517e830a3f7a5fe1dbcb65f24e33310cb7be7c
Yehu Chen <chenyehu@wustl.edu>
2021-10-15 02:21:53 -0500

    setting 0.9999=1

--- data/measures.R
+++ data/measures.R
@@ -107,32 +107,33 @@ for(i in 1:length(MODELS)){
 # perform paired t-test
 
 # print(paste("correlation: ", RHO , sep=""))
-# for (i in 2:length(MODELS)) {
-#   tmp = t.test(LL[,1], LL[, i], paired = TRUE)
-#   p = tmp[["p.value"]]
-#   print(tmp)
-# }
-# print(paste("correlation: ", RHO , sep=""))
-# for (i in c(1,2,4,5)) {
-#   # tmp = t.test(RMSE[,1], RMSE[, i], paired = TRUE)
-#   # p = tmp[["p.value"]]
-#   # if (p>=0.05){
-#   #   print(paste(MODELS[i], " not significant worse in RMSE.", sep=""))
-#   # }
-#   tmp = t.test(COVERAGE[,3], COVERAGE[, i], paired = TRUE)
-#   p = tmp[["p.value"]]
-#   print(tmp)
-#   if (p>=0.05){
-#     print(paste(MODELS[i], " not significant worse in COVERAGE.", sep=""))
-#   }
-#   # tmp = t.test(LL[,1], LL[, i], paired = TRUE)
-#   # p = tmp[["p.value"]]
-#   # if (p>=0.05){
-#   #   print(paste(MODELS[i], " not significant worse in LL.", sep=""))
-#   # }
-# }
-# 
-# q()
+for (i in 2:length(MODELS)) {
+  tmp = t.test(LL[,1], LL[, i], paired = TRUE)
+  p = tmp[["p.value"]]
+  print(tmp)
+  
+  tmp = t.test(RMSE[,1], RMSE[, i], paired = TRUE)
+  p = tmp[["p.value"]]
+  if (p>=0.05){
+    print(paste(MODELS[i], " not significant worse in RMSE.", sep=""))
+  }
+}
+print(paste("correlation: ", RHO , sep=""))
+for (i in c(1,2,3,4)) {
+  tmp = t.test(COVERAGE[,5], COVERAGE[, i], paired = TRUE)
+  p = tmp[["p.value"]]
+  print(tmp)
+  if (p>=0.05){
+    print(paste(MODELS[i], " not significant worse in COVERAGE.", sep=""))
+  }
+  # tmp = t.test(LL[,1], LL[, i], paired = TRUE)
+  # p = tmp[["p.value"]]
+  # if (p>=0.05){
+  #   print(paste(MODELS[i], " not significant worse in LL.", sep=""))
+  # }
+}
+
+q()
 
 ENORMSE = colMeans(ENORMSE)
 RMSE = colMeans(RMSE)
