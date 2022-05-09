dc14d2acf37126ee54a059a10a20ecee1341750c
Yehu Chen <chenyehu@wustl.edu>
2022-05-09 15:32:57 -0500

    error bar

--- data/measures.R
+++ data/measures.R
@@ -58,7 +58,7 @@ ll_score = function(true_effects, est_effects, pstd){
 MODELS = c("fullbayes", "multigp", "naivecf", "whitenoise", "whitenoisegroup","uncorreffecttrend",
            "grouptrend", "ife", "tfe", "blr", "cmgp", "bgsc", "perfectcor", "naiveICM", "unittrend","individual")
 
-# MODELS = c("fullbayes",  "ife", "tfe", "cmgp", "bgsc", "naiveICM", "unittrend","individual")
+MODELS = c("fullbayes",  "ife", "tfe", "cmgp", "bgsc", "naiveICM", "unittrend","individual")
 
 
 ENORMSE = matrix(0, nrow = MAXSEED, ncol=length(MODELS))
